77ab0e0d2a67628009c94802a7a8ebfd2117a325
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-10-08 18:58:55 -0300

    - Projeto inicial API

---
+++ WebApplication1/Controllers/Animais.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimaisController : Controller
+{
+    private static List<AnimalResultModel> Animais = new List<AnimalResultModel>()
+    {
+        new AnimalResultModel()
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Pedrinho",
+            IdadeEmMeses = 8,
+            Cor = "Cinza"
+        },
+        new AnimalResultModel()
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Joção",
+            IdadeEmMeses = 8,
+            Cor = "Caramelo"
+        }
+    };
+
+    [HttpGet("")]
+    public IEnumerable<AnimalResultModel> GetAnimais()
+    {
+        return Animais;
+    }
+
+    [HttpPost("")]
+    public Guid PostAnimal(AnimalRequestModel animal)
+    {
+        var animalAux = new AnimalResultModel()
+        {
+            Id = Guid.NewGuid(),
+            Cor = animal.Cor,
+            IdadeEmMeses = animal.IdadeEmMeses,
+            Nome = animal.Nome
+        };
+        Animais.Add(animalAux);
+        return animalAux.Id;
+    }
+
+    [HttpPut("{id}")]
+    public AnimalResultModel PutAnimal(Guid id, AnimalRequestModel animal)
+    {
+        var animalAux = Animais.First(animal => animal.Id == id);
+        animalAux.Nome = animal.Nome;
+        animalAux.IdadeEmMeses = animal.IdadeEmMeses;
+        animalAux.Cor = animal.Cor;
+        return animalAux;
+    }
+
+    [HttpDelete("{id}")]
+    public bool PutDelete(Guid id)
+    {
+        var animalAux = Animais.First(animal => animal.Id == id);
+        Animais.Remove(animalAux);
+        return true;
+    }
+}
---
+++ WebApplication1/Model/Animais.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Model;
+
+public class AnimalResultModel
+{
+    public Guid Id { get; set; }
+    public string Cor { get; set; }
+    public int IdadeEmMeses { get; set; }
+    public string Nome { get; set; }
+}
+
+public class AnimalRequestModel
+{
+    public string Cor { get; set; }
+    public int IdadeEmMeses { get; set; }
+    public string Nome { get; set; }
+}
